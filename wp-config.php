8e44ae1503f48f2aff436d43151bf96c45ffd4bc
ssweriduk <ssweriduk@gmail.com>
2017-04-21 15:13:10 -0400

    Added pingback denial

--- wp-config.php
+++ wp-config.php
@@ -15,3 +15,19 @@ if (!defined('ABSPATH'))
     define('ABSPATH', dirname(__FILE__) . '/');
 
 require_once(ABSPATH . 'wp-settings.php');
+
+
+//  Disable pingback.ping xmlrpc method to prevent Wordpress from participating in DDoS attacks
+//  More info at: https://docs.bitnami.com/?page=apps&name=wordpress&section=how-to-re-enable-the-xml-rpc-pingback-feature
+
+// remove x-pingback HTTP header
+add_filter('wp_headers', function($headers) {
+    unset($headers['X-Pingback']);
+    return $headers;
+});
+// disable pingbacks
+add_filter( 'xmlrpc_methods', function( $methods ) {
+        unset( $methods['pingback.ping'] );
+        return $methods;
+});
+add_filter( 'auto_update_translation', '__return_false' );
